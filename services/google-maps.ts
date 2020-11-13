fe087cfe6b796d5813d0ee1dd9daf7cf60cd9d1c
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-11-13 16:27:07 +0100

    refactor GoogleMapsService to use SDK instead of axios

--- services/google-maps.ts
+++ services/google-maps.ts
@@ -1,25 +1,59 @@
-import axios from 'axios'
+import { Place } from '../types';
 
-export default class GoogleMapsService {
-    private static googleApiBaseUrl = "";
-    private static googleApiToken: string | null = null;
 
-    public static setToken(token: string) {
-        this.googleApiToken = token;
+export class GoogleMapsService {
+    public static mapService() {
+        const map = new google.maps.Map(document.createElement('div'));
+        const mapsService = new google.maps.places.PlacesService(map);
+        return mapsService;
     }
 
-    public static searchPlace(query: string, fields = ['formatted_address', 'geometry']) {
-        return new Promise((resolve) => {
-            axios.get(
-                this.googleApiBaseUrl + '/place/findplacefromtext/json?'
-                + 'input=' + encodeURIComponent(query)
-                + '&fields=' + fields.join(',')
-                + '&key=' + this.googleApiToken
-                + '&inputtype=textquery'
-            )
-                .then(response => {
-                    resolve(response.data.candidates)
-                })
+    public static searchPlace(query: string, fields = ['formatted_address', 'geometry']): Promise<Place[]> {
+        return new Promise(resolve => {
+            this.mapService().findPlaceFromQuery({ query, fields }, (results, status) => {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    resolve(
+                        results.map(result => {
+                            if (!result.formatted_address || !result.geometry) {
+                                throw "Wrong response from google API...";
+                            }
+
+                            return {
+                                name: result.formatted_address,
+                                location: {
+                                    lat: result.geometry.location.lat(),
+                                    lng: result.geometry.location.lng()
+                                }
+                            }
+                        })
+                    );
+                }
+            })
         })
     }
+
+    public static createMap(options: { mapRoot: HTMLElement; place: Place; center: Place }) {
+        console.log('place', options.place, 'geometry', location)
+        if (location) {
+            const centerLatLng = new google.maps.LatLng(options.place.location.lat, options.place.location.lng);
+            console.log(centerLatLng)
+
+            new google.maps.Map(options.mapRoot, {
+                center: centerLatLng,
+                zoom: 15,
+            });
+        }
+
+
+
+        /*
+        const map = new google.maps.Map(options.mapRoot, {
+            center: options.place.geometry?.location,
+            zoom: 14
+        });
+        return map;
+        */
+    }
 }
+
+export default GoogleMapsService
